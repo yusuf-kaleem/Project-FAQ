0542eebdfcc227a90189d7f2c22e66fc46c272e5
yusuf Kaleem Khan <yusufkaleemkhan@yusufs-MacBook-Air.local>
2022-01-30 16:58:00 +0530

    adding global style

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
+import GlobalStyle from '../styles/index'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
