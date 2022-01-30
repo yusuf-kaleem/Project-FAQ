32dc7fbb79d224fef7ceeec9e4ec462e9e625e9e
yusuf Kaleem Khan <yusufkaleemkhan@yusufs-MacBook-Air.local>
2022-01-30 11:36:04 +0530

    layout update

--- frontend/components/layout/index.jsx
+++ frontend/components/layout/index.jsx
@@ -1,44 +1,17 @@
 import {
-        mainHeader,
-        mainFooter,
-        mainContent,
-        headerContent,
-        layoutBody,
-        headerSvg,
-        footerContactUs,
-        footerSvg
+
+    mainContent,
+    layoutBody,
+  
 } from './layout.module.css';
 
-import LieferandoLogo from './LieferandoLogo';
+import React from 'react'
 
 export default function Layout (props) {
-    return (
-    <div className={layoutBody}>
-        <header className={mainHeader}>
-        <svg className={headerSvg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-            <polygon fill="white" points="0,100 100,0 100,100"/>
-        </svg>
-        <div className={headerContent}>
-            <LieferandoLogo />
-        </div>
+return (
+<div className={layoutBody}>
+ 
 
-        </header>
-
-        <div className={mainContent}>
-        {props.children}
-        </div>
-        <div className={footerContactUs}>
-        <svg className={footerSvg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-            <polygon fill="white" points="100,0 0,0 0,100"/>
-        </svg>
-        <div>
-        <h3>Couldn't find what you are looking for?</h3>
-        <a href="">Contact us directly</a>
-        </div>
-        </div>
-        <footer className={mainFooter}>
-            Takeaway.com
-        </footer>
-    </div>
-    );
+</div>
+);
 }
